be3e8ceb7b64780ba2b7c23aa961c858108f7c29
YuriShido <sydonce57@gmail.com>
2021-08-21 16:10:26 -0700

    add client url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", ["http://localhost:3000","https://find-web-job-tweets.netlify.app/"]);
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next()
 });
